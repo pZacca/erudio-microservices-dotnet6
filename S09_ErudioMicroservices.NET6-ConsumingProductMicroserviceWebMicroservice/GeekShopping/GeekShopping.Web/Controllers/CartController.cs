eca07be001560db50f78772534c81c6bb6bff0a9
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-05-17 17:19:21 -0300

    Removing products from cart added

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceWebMicroservice/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceWebMicroservice/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -21,8 +21,20 @@ namespace GeekShopping.Web.Controllers
         [Authorize]
         public async Task<IActionResult> CartIndex()
         {
-
             return View(await FindUserCart());
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.RemoveFromCart(id, token);
+            if(response)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
         }
 
         private async Task<CartViewModel> FindUserCart()
