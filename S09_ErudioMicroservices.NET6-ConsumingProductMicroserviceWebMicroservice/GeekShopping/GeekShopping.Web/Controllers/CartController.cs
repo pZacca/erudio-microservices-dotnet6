9c3ff1ca40b17f2937eb2c3288c54a98884e36be
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-05-17 20:04:37 -0300

    Checkout screen added

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceWebMicroservice/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceWebMicroservice/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -70,6 +70,12 @@ namespace GeekShopping.Web.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Checkout()
+        {
+            return View(await FindUserCart());
+        }
+
         private async Task<CartViewModel> FindUserCart()
         {
             var token = await HttpContext.GetTokenAsync("access_token");
