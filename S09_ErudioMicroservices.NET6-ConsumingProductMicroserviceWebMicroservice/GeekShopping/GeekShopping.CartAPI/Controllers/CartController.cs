077add09fdaf138d52016f444f82b860270670f2
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-05-18 22:59:04 -0300

    Checkout Implemented

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceWebMicroservice/GeekShopping/GeekShopping.CartAPI/Controllers/CartController.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceWebMicroservice/GeekShopping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -62,6 +63,19 @@ namespace GeekShopping.CartAPI.Controllers
             var status = await _repository.RemoveCoupon(userId);
             if (!status) return NotFound();
             return Ok(status);
+        }
+
+        [HttpPost("checkout")]
+        public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
+        {
+            var cart = await _repository.FindCartByUserId(vo.UserId);
+            if (cart == null) return NotFound();
+            vo.CartDetails = cart.CartDetails;
+            vo.DateTime = DateTime.Now;
+
+            //TASK RabbitMQ logic comes here!!!
+
+            return Ok(vo);
         }
     }
 }
